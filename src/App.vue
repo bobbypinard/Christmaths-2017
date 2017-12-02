0d0f3303d7b14b89d88b43bf62ca06f6203c2684
bobbypinard <bobby.pinard@gmail.com>
2017-12-02 13:12:19 -0600

    Added scaffolding.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1>Add some layers to the Fibonacci tree.</h1>
+    <div class="form-group">
+      <b-button @click="addLayer" variant="default" name="button">Add layer</b-button>
+      <b-button @click="layer = 0" variant="default" name="button">Reset</b-button>
+    </div>
+
+    {{ layer }}  <!-- Testing -->
+    <div class="star">
+      <h1>Ho</h1>
+    </div>
+    <div class="tree">
+      {{ text }}
+    </div>
   </div>
 </template>
 
@@ -24,37 +21,46 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      layer: 0
+    }
+  },
+  methods: {
+    addLayer: function() {
+      return this.layer += 1
+    }
+  },
+  computed: {
+    text: function() {
+      if (this.layer >= 2) {
+        return "Ho Ho"
+      } else if (this.layer >= 1) {
+        return "Ho"
+      } else if (this.layer >= 0) {
+        return "Ho"
+      } else {
+        return "Error"
+      }
     }
   }
 }
 </script>
 
 <style lang="scss">
+body {
+  background-color: red;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #FFF;
   margin-top: 60px;
 }
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.tree {
+  font-size: 1.5em;
+  color: #7FFF00;
 }
 </style>
